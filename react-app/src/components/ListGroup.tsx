bb16944422afae0903458cea6ac41ff118a16299
kanyi <jkanyi757@gmail.com>
2024-03-08 16:33:02 +0300

    useState

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,9 +1,34 @@
-const ListGroup = () => {
-  return (
-    <div>
-      <h1>ListGroup</h1>
-    </div>
-  );
-}
+/** @format */
 
-export default ListGroup
+import { useState } from "react";
+
+const ListGroup = () => {
+  const [selectedIndex, setSelectedIndex] = useState(-1)
+  
+  let countries = [
+    'SanFrancisco',
+    'NewYork',
+    'LosAngeles',
+    'Chicago',
+    'Houston',
+  ];
+
+  return (
+    <>
+      <h1>List</h1>
+      {countries.length === 0 && <p>No countries found</p>}
+      <ul className="list-group">
+        {countries.map((country, index) => (
+          <li
+            className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+            key={index}
+            onClick={() => {setSelectedIndex(index)}}>
+            {country}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ListGroup;
